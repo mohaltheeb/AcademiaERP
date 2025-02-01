0f0e01288c0e69e0e0465f5e5ea70091610ee3cb
Mohammed Altheeb <altheebinfo@gmail.com>
2025-02-01 21:28:09 +0300

    feat(academia): Add a new feature when adding a new course the user is transferred to add course specification through a message

--- academia/academia/doctype/course/course.js
+++ academia/academia/doctype/course/course.js
@@ -2,21 +2,42 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Course", {
-	faculty: function(frm) {
-        var faculty = frm.doc.faculty;
-        frm.set_query("program", function() {
-            return {
-                "filters": {
-                    "faculty": faculty
-                }
-            };
-        });
-        frm.set_value('program', '');
-    },
-    before_save: function(frm){
-        if(frm.doc.course_type === 'University Requirement' || frm.doc.course_type === 'Faculty Requirement'){
-            frm.set_value('program', '');
-        }
-            
-    }
+	faculty: function (frm) {
+		var faculty = frm.doc.faculty;
+		frm.set_query("program", function () {
+			return {
+				filters: {
+					faculty: faculty,
+				},
+			};
+		});
+		frm.set_value("program", "");
+	},
+	before_save: function (frm) {
+		if (
+			frm.doc.course_type === "University Requirement" ||
+			frm.doc.course_type === "Faculty Requirement"
+		) {
+			frm.set_value("program", "");
+		}
+	},
+	after_save: function (frm) {
+		// التأكد من أن السجل جديد (عند إنشاء السجل تكون قيمة creation مساوية لـ modified)
+		if (frm.doc.creation === frm.doc.modified) {
+			frappe.msgprint({
+				title: __("Attention"),
+				indicator: "orange",
+				message: __(
+					"You must add a course specification for the course you just created."
+				),
+				primary_action: {
+					label: __("Go to Course Specification"),
+					action: function () {
+						// إنشاء سجل جديد في doctype "Course Specification" مع تمرير قيمة حقل course_code
+						frappe.new_doc("Course Specification", { course_code: frm.doc.name });
+					},
+				},
+			});
+		}
+	},
 });
