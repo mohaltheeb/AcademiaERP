4e00f5dbb4eec305d0fa354f199676119f0b2732
Mohammed Altheeb <altheebinfo@gmail.com>
2025-02-10 00:26:30 +0300

    feat(academia): Add filters for the tables in faculty requirement doctype

--- academia/academia/doctype/faculty_requirement/faculty_requirement.js
+++ academia/academia/doctype/faculty_requirement/faculty_requirement.js
@@ -6,3 +6,31 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Study Plan Course", {
+	required_courses_add: function (frm) {
+		frm.fields_dict["required_courses"].grid.get_field("course_code").get_query = function (
+			doc
+		) {
+			var courses_list = [];
+			$.each(doc.required_courses, function (idx, val) {
+				if (val.course_code) courses_list.push(val.course_code);
+			});
+			return { filters: [["Course Specification", "name", "not in", courses_list]] };
+		};
+	},
+});
+
+frappe.ui.form.on("Program Elective Course", {
+	elective_courses_add: function (frm) {
+		frm.fields_dict["elective_courses"].grid.get_field("course_code").get_query = function (
+			doc
+		) {
+			var courses_list = [];
+			$.each(doc.elective_courses, function (idx, val) {
+				if (val.course_code) courses_list.push(val.course_code);
+			});
+			return { filters: [["Course Specification", "name", "not in", courses_list]] };
+		};
+	},
+});
